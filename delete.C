8ec1909301a02ac86dc58cd960d52ec5fe8d3c3d
benkang <benjaminkang887@gmail.com>
2025-12-14 13:15:19 -0600

    fix delete by switching the type

--- delete.C
+++ delete.C
@@ -21,20 +21,36 @@ const Status QU_Delete(const string & relation,
 	AttrDesc attrDesc;
 
 	status = attrCat->getInfo(relation, attrName, attrDesc);
-
 	// Create scanner on the relation
 	HeapFileScan scanner(relation, status);
 	if (status != OK) { return status; };
-	
+
+	const char *filter;
+ 	switch (type) {
+            case INTEGER:{
+                int tmpInt = atoi(attrValue);
+                filter = (char *) &tmpInt;
+                break;
+	    }
+            case FLOAT:{
+                float tmpFloat = atof(attrValue);
+                filter = (char *) &tmpFloat;
+                break;
+	    }
+            case STRING:{
+                filter = attrValue;
+                break;
+	    }
+        }	
+
 	// Initiate scan
-	status = scanner.startScan(attrDesc.attrOffset,attrDesc.attrLen,type,attrValue,op);
+	status = scanner.startScan(attrDesc.attrOffset,attrDesc.attrLen,type,filter,op);
 	if (status != OK) { return status; }
 	
 	RID dummy;
 	while (scanner.scanNext(dummy) == OK){
 		scanner.deleteRecord();
 	}
-	scanner.endScan();
 	return OK;
 }
 
